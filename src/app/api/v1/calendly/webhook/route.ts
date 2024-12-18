51fbdd64494b30d7ef88295f6fb82059a32259e2
Laurin-Notemann <laurin.notemann@gmail.com>
2024-12-18 15:37:49 +0100

    change to post

--- src/app/api/v1/calendly/webhook/route.ts
+++ src/app/api/v1/calendly/webhook/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
   console.log("heher")
   return NextResponse.json("ok");
 }
