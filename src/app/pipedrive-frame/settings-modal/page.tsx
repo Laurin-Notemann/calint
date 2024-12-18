dd6ab2a95e740fde5bb55c2686073911f05fc9f1
Laurin-Notemann <laurin.notemann@gmail.com>
2024-12-18 19:02:40 +0100

    debug

--- src/app/pipedrive-frame/settings-modal/page.tsx
+++ src/app/pipedrive-frame/settings-modal/page.tsx
@@ -7,15 +7,14 @@ export default function PipedriveFrame() {
   useEffect(() => {
     const initializePipedrive = async () => {
       try {
-        const sdk = await new AppExtensionsSDK({ identifier: "e8ac57d1-75e0-4660-9556-339baebaeb0d" }).initialize({ size: { height: 500 } });
-        // You can access Pipedrive context here
-        const settings = sdk.userSettings;
-        console.log('Pipedrive context:', settings);
-        //        const { status } = await sdk.execute(Command.OPEN_MODAL, {
-        //          type: Modal.JSON_MODAL,
-        //          action_id: "Calint Settings"
-        //        })
-        //console.log('status', status);
+        const sdk = await new AppExtensionsSDK().initialize();
+        //        const settings = sdk.userSettings;
+        //        console.log('Pipedrive context:', settings);
+        //        //        const { status } = await sdk.execute(Command.OPEN_MODAL, {
+        //        //          type: Modal.JSON_MODAL,
+        //        //          action_id: "Calint Settings"
+        //        //        })
+        //        //console.log('status', status);
       } catch (error) {
         console.error('Failed to initialize Pipedrive SDK:', error);
       }
