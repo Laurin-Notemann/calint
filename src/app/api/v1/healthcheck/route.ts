29765dcd7146af17346df66a52a612d0a525154c
Laurin-Notemann <laurin.notemann@gmail.com>
2025-02-17 17:16:21 +0100

    nervnicht

--- src/app/api/v1/healthcheck/route.ts
+++ src/app/api/v1/healthcheck/route.ts
@@ -3,11 +3,12 @@ import { NextRequest, NextResponse } from "next/server";
 
 const loggerr = createLogger("Healthcheck")
 export async function GET(request: NextRequest) {
+  loggerr.error("Hallo?")
   const test = new URL(request.referrer)
 
   loggerr.error(test)
 
   loggerr.error(test.searchParams.get("userId"))
 
-  return NextResponse.json("Ok");
+  return NextResponse.json("Ok" + test.searchParams.get("userId"));
 }
