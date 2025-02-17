783a993ab4b167e9676b5cdf2917b92a19c26381
Laurin-Notemann <laurin.notemann@gmail.com>
2025-02-17 17:08:39 +0100

    test

--- src/app/api/v1/healthcheck/route.ts
+++ src/app/api/v1/healthcheck/route.ts
@@ -1,5 +1,11 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const test = new URL(request.referrer)
+
+  console.log(test)
+
+  console.log(test.searchParams.get("userId"))
+
   return NextResponse.json("Ok");
 }
