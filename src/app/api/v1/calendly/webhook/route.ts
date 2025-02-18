1014e432d7eb000d90c854ad5dd3cb5014dae56f
Laurin-Notemann <laurin.notemann@gmail.com>
2025-02-18 11:33:12 +0100

    log

--- src/app/api/v1/calendly/webhook/route.ts
+++ src/app/api/v1/calendly/webhook/route.ts
@@ -4,6 +4,9 @@ import { createLogger } from "@/utils/logger";
 const logger = createLogger("calendly-webhook");
 
 export async function POST(request: NextRequest) {
+  logger.info(JSON.stringify(await request.json()))
+  logger.info(JSON.stringify(request.headers))
+  logger.info(JSON.stringify(request.nextUrl))
   logger.info("Received Calendly webhook");
   return NextResponse.json("ok");
 }
