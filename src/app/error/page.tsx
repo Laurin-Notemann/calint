62349cdffeb81155d42bc26e661784881f764f2a
Laurin-Notemann <laurin.notemann@gmail.com>
2025-02-21 10:33:50 +0100

    fix

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -6,7 +6,7 @@ export default async function ErrorPage({
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   const errorMsg = searchParams["error-msg"];
-  console.log("Error from params:", errorMsg);
+  console.log("Error from params:", JSON.stringify(errorMsg));
 
   return <div>{errorMsg || "Could not use OAuth to login with Pipedrive"}</div>;
 }
